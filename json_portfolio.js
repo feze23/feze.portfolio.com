f78c28fb1936761eac9a6ec5b438b7d6fc00f65b
Steve Edgar Feze Feugang <112581035+Rute23@users.noreply.github.com>
2022-12-04 21:05:19 +0100

    Add files via upload

---
+++ json_portfolio.js
@@ -0,0 +1,89 @@
+
+    var tutorbot = document.getElementById('tutorbot');
+    var faqbot = document.getElementById('faqbot');
+    var phdproject = document.getElementById('phdproject');
+    var nlp = document.getElementById('nlp');
+    var section1 = document.getElementById('section1');
+    var section2 = document.getElementById('section2');
+    var section3 = document.getElementById('section3');
+    var comp = document.getElementById('comp');
+    var skill = document.getElementById('skill');
+    var phdmenu = document.getElementById('phdproject');
+    var section4 = document.getElementById('section4');
+    var section5 =document.getElementById('section5');
+    var spacyMenu = document.getElementById('nlp')
+    var section6 = document.getElementById('section6');
+    var formulaire = document.getElementById('formular');
+    var competence = document.getElementById('comp');
+
+
+    tutorbot.addEventListener('click', function(e){
+        //alert("bonjour");
+        section4.style.display = "none";
+        section1.style.display = "none";
+        section3.style.display = "none";
+        section2.style.display="block";
+        section5.style.display = "none";
+        section6.style.display = "none";
+        //console.log(section2.innerHTML);
+    }, false);
+
+    faqbot.addEventListener('click', function(e){
+        section4.style.display = "none";
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "block";
+        section5.style.display = "none";
+        section6.style.display = "none";
+    }, false);
+
+    comp.addEventListener('click', function(e){
+        section4.style.display = "none";
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        skill.style.display = "block";
+        section5.style.display = "none";
+        section6.style.display = "none";
+    })
+
+    competence.addEventListener('click', function(e){
+        section4.style.display = "none";
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        section5.style.display = "none";
+        section6.style.display = "none";
+        skill.style.display = "block";
+    })
+
+
+    phdmenu.addEventListener('click', function(e){
+        section5.style.display = "none";
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        skill.style.display = "none";
+        section4.style.display = "block";
+        section6.style.display = "none";
+        
+    })
+
+    spacyMenu.addEventListener('click', function(e){
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        skill.style.display = "none";
+        section4.style.display = "none";
+        section5.style.display = "block";
+        section6.style.display = "none";
+    })
+    formulaire.addEventListener('click', function(e){
+        section1.style.display="none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        skill.style.display = "none";
+        section4.style.display = "none";
+        section5.style.display = "none";
+        section6.style.display = "block";
+    })
